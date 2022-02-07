1a6d659903c630bdd5eecc351f667167204dc6f8
ArdzaRicho <bullsdog32739@gmail.com>
2022-02-07 11:40:28 +0700

    modul 1

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('Home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+    "nama" => "Ardzaricho R",
+    "email" => "bullsdog32739@gmail.com",
+    "gambar" => "gambarku.jpg"
+    ]);
+});
+    
+Route::get('/gallery', function () {
+        return view('Gallery', [
+            "nama" => "Ardzaricho R",
+            "gambar" => "gambarku.jpg"
+        ]);
+
+});
